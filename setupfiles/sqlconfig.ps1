468941f180dd8bcc692b122d17692350bc2f9bbb
mdalal-accolite <mohit.dalal@accolitedigital.com>
2024-06-25 17:17:20 +0530

    Add files via upload

--- setupfiles/sqlconfig.ps1
+++ setupfiles/sqlconfig.ps1
@@ -1,4 +1,4 @@
-robocopy  '\\172.20.32.12\Software\ansibleStaging\MSSQL Automation\2016'  'C:\MSSQL2016\'
+robocopy  '\\172.20.32.12\Software\ansibleStaging\MSSQL Automation\2017'  'C:\MSSQL2017\'
 robocopy  '\\172.20.32.12\Software\Software\Microsoft Software\SSMS' 'C:\SSMS\'
-C:\MSSQL2016\en_sql_server_2016_enterprise_with_service_pack_2_x64_dvd_12124051.iso
+C:\MSSQL2017\SW_DVD9_SQL_Svr_Enterprise_Edtn_2017_64Bit_English_MLF_X21-56962.ISO
 sllep -s 5
