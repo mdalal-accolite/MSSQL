eeae75692f2d30bf150b48d9a459830eac57f413
mdalal-accolite <mohit.dalal@accolitedigital.com>
2024-06-26 12:44:26 +0530

    Update sqlconfig.ps1

--- setupfiles/sqlconfig.ps1
+++ setupfiles/sqlconfig.ps1
@@ -1,4 +1,4 @@
 robocopy  '\\172.20.32.12\Software\ansibleStaging\MSSQL Automation\2019'  'C:\MSSQL2019\'
 robocopy  '\\172.20.32.12\Software\Software\Microsoft Software\SSMS' 'C:\SSMS\'
 Mount-DiskImage -ImagePath "C:\MSSQL2019\SW_DVD9_SQL_Svr_Enterprise_Edtn_2019Dec2019_64Bit_English_MLF_X22-22247.ISO"
-sllep -s 20
+sleep -s 20
