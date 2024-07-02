5e1c49a282b5820c4436d8185324bb62fdae4f68
mdalal-accolite <mohit.dalal@accolitedigital.com>
2024-07-02 12:13:47 +0530

    Update sqlconfig.ps1

--- setupfiles/sqlconfig.ps1
+++ setupfiles/sqlconfig.ps1
@@ -1,4 +1,4 @@
 robocopy  '\\172.20.32.12\Software\ansibleStaging\MSSQL Automation\2017'  'C:\MSSQL2017\'
 robocopy  '\\172.20.32.12\Software\Software\Microsoft Software\SSMS' 'C:\SSMS\'
 C:\MSSQL2017\SW_DVD9_SQL_Svr_Enterprise_Edtn_2017_64Bit_English_MLF_X21-56962.ISO
-sllep -s 5
+sleep -s 5
